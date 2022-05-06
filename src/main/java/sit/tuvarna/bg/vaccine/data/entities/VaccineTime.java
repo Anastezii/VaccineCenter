f841f942da98f7ac28923875a141aa185a672320
Berna <bebozcan@gmail.com>
2022-05-06 10:35:33 +0300

    Entity VaccineTime updated

--- src/main/java/sit/tuvarna/bg/vaccine/data/entities/VaccineTime.java
+++ src/main/java/sit/tuvarna/bg/vaccine/data/entities/VaccineTime.java
@@ -22,14 +22,87 @@ public class VaccineTime implements Serializable {
     private String vaccine_price;
 
     @ManyToOne
-    @Column(name="pet",nullable = false)
+    @JoinColumn(name="pet",nullable = false)
     private Pet pet;
 
     @ManyToOne
-    @Column(name="client",nullable = false)
+    @JoinColumn(name="client",nullable = false)
     private Client client;
 
     @ManyToOne
-    @Column(name="veterinarian",nullable = false)
+    @JoinColumn(name="veterinarian",nullable = false)
     private Veterinarian veterinarian;
+
+
+    public VaccineTime() {
+    }
+
+    public VaccineTime(Long id_user, String vaccine_name, String vaccine_price, Pet pet, Client client, Veterinarian veterinarian) {
+        this.id_user = id_user;
+        this.vaccine_name = vaccine_name;
+        this.vaccine_price = vaccine_price;
+        this.pet = pet;
+        this.client = client;
+        this.veterinarian = veterinarian;
+    }
+
+    public Long getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(Long id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getVaccine_name() {
+        return vaccine_name;
+    }
+
+    public void setVaccine_name(String vaccine_name) {
+        this.vaccine_name = vaccine_name;
+    }
+
+    public String getVaccine_price() {
+        return vaccine_price;
+    }
+
+    public void setVaccine_price(String vaccine_price) {
+        this.vaccine_price = vaccine_price;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Veterinarian getVeterinarian() {
+        return veterinarian;
+    }
+
+    public void setVeterinarian(Veterinarian veterinarian) {
+        this.veterinarian = veterinarian;
+    }
+
+    @Override
+    public String toString() {
+        return "VaccineTime{" +
+                "id_user=" + id_user +
+                ", vaccine_name='" + vaccine_name + '\'' +
+                ", vaccine_price='" + vaccine_price + '\'' +
+                ", pet=" + pet +
+                ", client=" + client +
+                ", veterinarian=" + veterinarian +
+                '}';
+    }
 }
