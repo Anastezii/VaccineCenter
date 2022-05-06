f745c165c193a230970bdcf1d7182438349c25e1
Berna <bebozcan@gmail.com>
2022-05-06 10:55:41 +0300

    Repository for Pet added

---
+++ src/main/java/sit/tuvarna/bg/vaccine/data/repository/PetRepository.java
@@ -0,0 +1,106 @@
+package sit.tuvarna.bg.vaccine.data.repository;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import sit.tuvarna.bg.vaccine.data.acces.Connection;
+import sit.tuvarna.bg.vaccine.data.entities.Pet;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PetRepository implements DAORepository<Pet> {
+
+    private static final Logger log = Logger.getLogger(PetRepository.class);
+
+    private static PetRepository getInstance() {
+        return PetRepository.PetRepositoryHolder.INSTANCE;
+    }
+
+    private static class PetRepositoryHolder {
+        public static PetRepository INSTANCE = new PetRepository();
+    }
+
+    @Override
+    public void save(Pet obj) {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.save(obj);
+            log.info("Pet was saved successfully");
+        } catch (Exception ex) {
+            log.error("Pet was not saved, error (" + ex.getCause());
+        } finally {
+            transaction.commit();
+        }
+        session.close();
+    }
+
+    @Override
+    public void update(Pet obj) {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.update(obj);
+            log.info("Pet was updated successfully");
+        } catch (Exception ex) {
+            log.error("Pet was not updated, error (" + ex.getCause());
+        } finally {
+            transaction.commit();
+        }
+        session.close();
+    }
+
+    @Override
+    public void delete(Pet obj) {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.delete(obj);
+            log.info("Pet was deleted successfully");
+        } catch (Exception ex) {
+            log.error("Pet was not deleted, error (" + ex.getCause());
+        } finally {
+            transaction.commit();
+        }
+        session.close();
+    }
+
+    @Override
+    public Pet getById(Long id) {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Pet> pets = new LinkedList<Pet>();
+        try {
+            String jpql = "SELECT p FROM Pet p WHERE id_pet =" + id;
+            pets.addAll(session.createQuery(jpql, Pet.class).getResultList());
+            log.info("Successfully gets all pets");
+
+        } catch (Exception ex) {
+            log.error("Get ig pet error : " + ex.getMessage());
+        } finally {
+            transaction.commit();
+        }
+        session.close();
+        return pets.get(0);
+    }
+
+    @Override
+    public List<Pet> getAll() {
+        Session session = Connection.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Pet> pets = new LinkedList<Pet>();
+        try {
+            String jpql = "SELECT p FROM Pet p";
+            pets.addAll(session.createQuery(jpql, Pet.class).getResultList());
+            log.info("Result all pets");
+
+        } catch (Exception ex) {
+            log.error("Get pet error : " + ex.getMessage());
+        } finally {
+            transaction.commit();
+        }
+        session.close();
+        return pets;
+    }
+}
